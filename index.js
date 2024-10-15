99a112326a3162a07559a43a1097ab647aef5242
indiodev <jhollyfer.fr@gmail.com>
2024-10-15 01:31:23 -0500

    initial setup

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-import { XLSXReadableStream } from "./readable.js";
+import { createReadStream } from "node:fs";
 import { JSONMapPropsTransform, XLSXToJSONTransform } from "./transform.js";
 import { ToJSONWritableStream } from "./writeable.js";
+// import { XLSXReadableStream } from "./readable.js";
+// import { JSONMapPropsTransform, XLSXToJSONTransform } from "./transform.js";
 
-new XLSXReadableStream()
+// new XLSXReadableStream()
+//   .pipe(new XLSXToJSONTransform())
+//   .pipe(new JSONMapPropsTransform())
+//   .pipe(new ToJSONWritableStream());
+
+const file_path = new URL("./input.xlsx", import.meta.url);
+
+const readable = createReadStream(file_path);
+
+readable
   .pipe(new XLSXToJSONTransform())
   .pipe(new JSONMapPropsTransform())
   .pipe(new ToJSONWritableStream());
